3c48a89ee35942e1ee7bbf9ca26c22768aedaf73
Calebe Oliveira <calebe81899026@gmail.com>
2024-07-01 22:30:42 -0300

    rename title of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Home } from "./pages/Home"
+import { About } from "./pages/Home/sections/About/About"
 
 function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   )
 }
