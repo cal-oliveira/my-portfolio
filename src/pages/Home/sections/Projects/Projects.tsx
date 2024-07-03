2a750a3adf9ee2478e33a1260b452d3bb97842f3
Calebe Oliveira <calebe81899026@gmail.com>
2024-07-02 23:59:17 -0300

    add project grid

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -14,29 +14,37 @@ export function Projects(){
         width: "100%",
         display: "flex",
         alignItems: "center",
-        
+
         justifyContent: "center",
         flexDirection: "column",
         textAlign: "center",
         borderRadius: "4px"
     }))
 
+    const StyledP = styled("p")(()=>({
+        color: "white",
+        textAlign: "start"
+    }))
+
     return(
         <>
             <StyledProject>
                 <Container>
-                  <Typography color={"white"} variant="h2" textAlign="center" padding={5}>Projects</Typography>
+                  <Typography color={"white"} variant="h2" textAlign="center" >Projects</Typography>
 
                   <Grid container spacing={4}>
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={6}>
                         <StyledBox>
-
+                            <Typography color={"white"} fontSize={28}>Cronômetro</Typography>
+                            <img src="https://github.com/cal-oliveira/Cronometro/blob/main/screenshots/img--1.png?raw=true" width="100%" alt="" />
+                            <StyledP>Cronômetro desenvolvido com HTML, CSS e JS <br /> Design responsivo e otimizado para dispositivos móveis. <br />Projeto hospedado no github pages</StyledP>
                         </StyledBox>
                     </Grid>
 
-                    <Grid item md={6}>
+                    <Grid item xs={12} md={6}>
                         <StyledBox>
-                            
+                            <Typography color={"white"} fontSize={28}>Snake</Typography>
+                            <img src="" alt="" />
                         </StyledBox>
                     </Grid>
                   </Grid>
