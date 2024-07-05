f988f4c87259b7ecdb2d71f09fbab5d47ef5ca75
Calebe Oliveira <calebe81899026@gmail.com>
2024-07-05 18:19:14 -0300

    adding animation to section projects

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -1,28 +1,27 @@
 import { Container, Grid, styled } from "@mui/material"
 import Project from "../../../../components/Project"
 import { SectionTitle } from "../../../../components/SectionTitle"
+import { motion } from "framer-motion"
 
 export function Projects(){
 
     const StyledProject = styled("div")(({ theme })=>({
         backgroundColor: theme.palette.primary.main,
-        height: "100vh",
+        height: "auto",
         '@media (max-width: 600px)': {
             height: "auto",
         }
     }))
 
-    /* const StyledBox = styled(Box)(()=>({
-        border: "2px solid rgba(211,211,211,0.2)",
-        height: "50vh",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        textAlign: "center",
-        borderRadius: "4px"
-    })) */
+    const boxVariantsLeft = {
+        hidden: { opacity: 0, x: -200 },
+        visible: { opacity: 1, x: 0 }
+    }
+
+    const boxVariantsRight = {
+        hidden: { opacity: 0, x: 200 },
+        visible: { opacity: 1, x: 0 }
+    }
 
     return(
         <>
@@ -31,22 +30,71 @@ export function Projects(){
                   <SectionTitle color={"white"}>Projects</SectionTitle>
                   <Grid container spacing={4}>
                     <Grid item xs={12} md={6}>
-                        <Project 
-                            title="Cronômetro"
-                            urlImage="https://github.com/cal-oliveira/Cronometro/blob/main/screenshots/img--1.png?raw=true"
-                            description="Cronômetro desenvolvido com HTML, CSS e JS Design responsivo e otimizado para dispositivos móveis. Projeto hospedado no github pages"
-                            techs="JavaScript, HTML, CSS"
-                        />
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: false }}
+                            transition={{ duration: 1, delay: 0.2 }}
+                            variants={boxVariantsLeft}
+                        >
+                            <Project 
+                                title="Cronômetro"
+                                urlImage="https://github.com/cal-oliveira/Cronometro/blob/main/screenshots/img--1.png?raw=true"
+                                description="Cronômetro desenvolvido com HTML, CSS e JS Design responsivo e otimizado para dispositivos móveis. Projeto hospedado no github pages"
+                                techs="JavaScript, HTML, CSS"
+                            />
+                        </motion.div>
                     </Grid>
 
                     <Grid item xs={12} md={6}>
-                        <Project 
-                            title="PokeNext"
-                            urlImage="https://github.com/cal-oliveira/pokenext/blob/master/screenshots/pokenext.png?raw=true"
-                            description="Pokedex desenvolvido com NextJs e hospedado na Vercel"
-                            techs="JavaScript, HTML, CSS"
-                            
-                        />
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: false }}
+                            transition={{ duration: 1, delay: 0.2 }}
+                            variants={boxVariantsRight}
+                        >
+                            <Project 
+                                title="PokeNext"
+                                urlImage="https://github.com/cal-oliveira/pokenext/blob/master/screenshots/pokenext.png?raw=true"
+                                description="Pokedex desenvolvido com NextJs e hospedado na Vercel"
+                                techs="JavaScript, HTML, CSS"
+                            />
+                        </motion.div>
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: false }}
+                            transition={{ duration: 1, delay: 0.2 }}
+                            variants={boxVariantsLeft}
+                        >
+                            <Project 
+                                title="Cronômetro"
+                                urlImage="https://github.com/cal-oliveira/Cronometro/blob/main/screenshots/img--1.png?raw=true"
+                                description="Cronômetro desenvolvido com HTML, CSS e JS Design responsivo e otimizado para dispositivos móveis. Projeto hospedado no github pages"
+                                techs="JavaScript, HTML, CSS"
+                            />
+                        </motion.div>
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: false }}
+                            transition={{ duration: 1, delay: 0.2 }}
+                            variants={boxVariantsRight}
+                        >
+                            <Project 
+                                title="PokeNext"
+                                urlImage="https://github.com/cal-oliveira/pokenext/blob/master/screenshots/pokenext.png?raw=true"
+                                description="Pokedex desenvolvido com NextJs e hospedado na Vercel"
+                                techs="JavaScript, HTML, CSS"
+                            />
+                        </motion.div>
                     </Grid>
                   </Grid>
                 </Container>
