74dbdbfea4546dd680bc753a946ac2a65b167817
Calebe Oliveira <calebe81899026@gmail.com>
2024-07-04 23:14:25 -0300

    deleting comments

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -26,16 +26,6 @@ export function About(){
         textAlign: "center",
     }))
 
-    const StyledSkill = styled(Box)(()=>({
-        height: "3vh",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: "4px",
-        border: "1px solid rgba(0,0,0,0.2)"
-    }))
-
     return(
         <>
             <StyledAbout>
@@ -62,41 +52,6 @@ export function About(){
 
                     <p style={{ textAlign: "center", lineHeight: '1.5' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eligendi officiis eius facilis dolorum perspiciatis eos. Autem provident cupiditate in, amet quod sunt ut dicta exercitationem aliquam eum repellendus neque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta libero in vitae, provident nemo dolores, cum voluptatum minus veniam rem atque tempora ex optio officia vero nobis reprehenderit quibusdam architecto. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi corrupti, iste animi pariatur accusamus facere veniam et tempore incidunt ipsum harum assumenda magnam dolorem cum qui quae natus rerum deleniti.</p>
                     <hr />
-
-                    {/* <SectionTitle color={"black"}>Skills</SectionTitle>
-                    <Grid container display="flex" justifyContent="center" alignItems="center" spacing={2}> 
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">JavaScript</Typography>
-                            </StyledSkill>
-                        </Grid>
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">TypeScript</Typography>
-                            </StyledSkill>
-                        </Grid>
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">React</Typography>
-                            </StyledSkill>
-                        </Grid>
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">React Native</Typography>
-                            </StyledSkill>
-                        </Grid>
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">HTML</Typography>
-                            </StyledSkill>
-                        </Grid>
-                        <Grid item xs={4} md={2}>
-                            <StyledSkill>
-                                <Typography fontWeight="500">CSS</Typography>
-                            </StyledSkill>
-                        </Grid>
-                    </Grid> */}
-
                 </Container>
             </StyledAbout>
         </>
